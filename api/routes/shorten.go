8cdaaeea91afa914b92a7e012dd9b63d92564c8a
AayushShah <aayushshah1142@gmail.com>
2024-02-04 14:06:20 +0530

    completed shortening logic + dockerfile

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -33,7 +33,7 @@ type response struct {
 	URL             string        `json:"url"`
 	CustomShort     string        `json:"short"`
 	Expiry          time.Duration `json:"expiry"`
-	XRateRemaining  int32         `json:"rate_limit"`
+	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"x_rate_limit_reset"`
 }
 
@@ -88,6 +88,7 @@ func ShortenURL(c *fiber.Ctx) error {
 	r := database.CreateClient(0)
 	defer r.Close()
 
+	//check if custom short is already in use
 	val, _ = r.Get(database.Ctx, id).Result()
 	if val != "" {
 		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": "URL custom short already in use"})
@@ -95,10 +96,23 @@ func ShortenURL(c *fiber.Ctx) error {
 	if body.Expiry == 0 {
 		body.Expiry = 24
 	}
+
 	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Unable to connect to server"})
 	}
+
+	resp := response{URL: body.URL, CustomShort: "", Expiry: body.Expiry, XRateRemaining: 10, XRateLimitReset: 30}
+
 	r2.Decr(database.Ctx, c.IP())
 
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+
+	return c.Status(fiber.StatusOK).JSON(resp)
 }
